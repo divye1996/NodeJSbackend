4b1176562b43b3d0a9bad03adacab1b2e982cf1c
divye1996 <56021140+divye1996@users.noreply.github.com>
2022-05-17 16:01:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('MongoDb_url');
+  mongoose.connect('localhost');
   mongoose.set('debug', true);
 }
 
